d2875908a13434f1ffe4bd199fee7dac5f07dad3
Anderson De Jesus Mejia Tapia <anderson_the_classic@hotmail.com>
2018-10-24 13:56:18 -0400

    fixed responsive stuff, added scroll to navigation... fixed icon--big media-query-phone. changed logo to p element... Removed border-bottom to nav a in phone mode

---
+++ resources/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    
+    // sticky nav
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+    
+    //show more btn to features
+    $('.js--scroll-to-features').click(function() {
+        $('html, header').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+    
+    /* Navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    
+});
